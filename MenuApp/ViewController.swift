19e578209e8a9e74e935d59bc637ce159e71294b
Vladimir <motiw@icloud.com>
2023-04-04 11:34:32 +0600

    Initial Commit

---
+++ MenuApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MenuApp
+//
+//  Created by Vladimir on 04.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
